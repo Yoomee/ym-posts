739021e592f86c109d597017f6d6cb9ba5654ca0
Ian Mooney <ian@yoomee.com>
2012-03-27 13:40:16 +0100

    Ajax image upload with remotipart

--- lib/ym_posts.rb
+++ lib/ym_posts.rb
@@ -5,3 +5,5 @@ end
 
 Dir[File.dirname(__FILE__) + '/ym_posts/models/*.rb'].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/ym_posts/controllers/*.rb'].each {|file| require file }
+
+require 'remotipart'
