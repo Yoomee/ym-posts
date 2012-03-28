627e1e415df91b6dc7ebf88be6eb62982a9bf0f4
Ian Mooney <ian@yoomee.com>
2012-03-28 13:09:42 +0100

    Added posts#index

--- lib/ym_posts/controllers/posts_controller.rb
+++ lib/ym_posts/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ module YmPosts::PostsController
   
   def self.included(base)
     base.expose(:post)
+    base.expose(:posts) {Post.page(params[:page])}
+    base.expose(:top_tags) {Tag.scoped}
   end
   
   def create
