aceaef84d39568b4b8ce25db31693d4ba3a406e0
Ian Mooney <ian@yoomee.com>
2012-04-13 14:23:19 +0100

    Changed abilities in install

--- lib/generators/ym_posts/install_generator.rb
+++ lib/generators/ym_posts/install_generator.rb
@@ -41,9 +41,12 @@ module YmPosts
         File.exists?("#{Rails.root}/app/models/ability.rb") && !File.open("#{Rails.root}/app/models/ability.rb").read.include?(model_name)
       end
 
-      def add_ability(role, abilities)
-        ability_string = [*abilities].join("\n      ")
-        insert_into_file "app/models/ability.rb", "\n      #{ability_string}", :after => "#{role} ability"
+      def add_ability(roles, abilities)
+        [*roles].each do |role|
+          tabbed_space = role==:open ? "\n    " : "\n      "
+          ability_string = tabbed_space + [*abilities].join(tabbed_space)
+          insert_into_file "app/models/ability.rb", ability_string, :after => "#{role} ability", :force => true
+        end
       end
       
     end
