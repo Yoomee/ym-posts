fa3c9f8807f2d2f67a1c12f2ba2cadd8e0a66195
Ian Mooney <ian@yoomee.com>
2013-10-21 12:48:26 +0100

    comments#destroy action and load_and_authorize_resource in controller

--- lib/ym_posts/controllers/comments_controller.rb
+++ lib/ym_posts/controllers/comments_controller.rb
@@ -1,7 +1,15 @@
 module YmPosts::CommentsController
   
+  def self.included(base)
+    base.load_and_authorize_resource :except => :create
+  end
+
   def create
     @comment = current_user.comments.create(params[:comment])
+  end
+
+  def destroy
+    @comment.destroy
   end
   
 end
