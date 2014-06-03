1cd3c46e61e8a0303f79a2975b4b645fe026f7a2
Edward Andrews-Hodgson <edward@yoomee.com>
2014-06-03 09:28:13 +0100

    Allow adding of permitted parameters By overriding permitted_post_parameters in client app

--- lib/generators/ym_posts/templates/controllers/posts_controller.rb
+++ lib/generators/ym_posts/templates/controllers/posts_controller.rb
@@ -1,3 +1,9 @@
 class PostsController < ApplicationController
   include YmPosts::PostsController
+
+  private
+  def permitted_post_parameters
+    %w(file image retained_file retained_image tag_list target_id target_type text video_url)
+  end
+
 end
--- lib/ym_posts/controllers/posts_controller.rb
+++ lib/ym_posts/controllers/posts_controller.rb
@@ -1,39 +1,39 @@
 module YmPosts::PostsController
-  
+
   def self.included(base)
     base.load_and_authorize_resource
   end
-  
+
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
       @new_post = Post.new(:target => @post.target, :user => @post.user)
     end
   end
-  
+
   def destroy
     @post.destroy
   end
-  
+
   def file
     send_file(@post.file_path, :filename => @post.file_name)
   end
 
   def index
-    @posts = Post.page(params[:page])    
+    @posts = Post.page(params[:page])
     @top_tags = Post.tag_counts_on(:tags, :limit => 10)
   end
-  
+
   def modal
     render :layout => false
   end
-  
+
   def show
   end
 
   private
   def post_params
-    params.require(:post).permit(:file, :image, :retained_image, :tag_list, :target_id, :target_type, :text, :video_url, :retained_file)
+    params.require(:post).permit(permitted_post_parameters)
   end
 
 end
