d97083352f022ddd6f492b59d722c85ffa46b832
Greg Woodcock <greg@yoomee.com>
2015-08-03 16:13:57 +0100

    Stop recording notifications after save as it is now done in elefriends

--- lib/ym_posts/models/comment.rb
+++ lib/ym_posts/models/comment.rb
@@ -3,19 +3,8 @@ module YmPosts::Comment
   def self.included(base)
     base.belongs_to(:user)
     base.belongs_to(:post)
-
     # base.send(:default_scope, base.order('created_at DESC'))
-
     base.validates(:user, :post, :text, :presence => true)
-    base.after_save :record_notifications
-
-  end
-
-  def record_notifications
-    users_to_notify = [post.user] + post.comments.collect(&:user)
-    users_to_notify.uniq!
-    users_to_notify = users_to_notify - [user]
-    users_to_notify.each { |user| Notification.create(:user => user, :resource => self, :resource_type => 'Comment') }
   end
 
   def to_s
